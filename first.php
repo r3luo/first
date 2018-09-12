aca56e9a848d50bc7e1b26a00f6913ceefaec1bb
ruidev <doyano@gmail.com>
2018-09-12 07:56:24 -0700

    First commit

---
+++ first.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>The first application to render random code</title>
+		<link rel="stylesheet" href="styles.css" />
+	</head>
+	<body>
+		<div class="lorem">Lorem...</div>
+	</body>
+</html>
